7f0b1dc1b289fb5146503746467292acd7979fba
avilisa <alesya.20030306@gmail.com>
2022-05-30 16:29:58 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import random
+random_list = [random.randint(1, 1000) for i in range(10000)]
+before = datetime.now()
+
+sorted_list = sorted(random_list)
+print(sorted_list)
+after_sorted = datetime.now()
+
+for i in range (10-1):
+    for j in range(10-i-1):
+        if random_list[j] > random_list[j+1]:
+            random_list[j], random_list[j+1] = random_list[j+1], random_list[j]
+
+print(random_list)
+after_bubble = datetime.now()
+
+print(after_sorted - before)
+print(after_bubble - after_sorted)
